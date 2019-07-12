d7a338812fd619575d875c1a9f9407c846d720d3
ggaeming@naver.com <ggaeming@naver.com>
2019-07-12 17:37:07 +0900

    190712

--- HelloCPP/HelloCPP/Hello.cpp
+++ HelloCPP/HelloCPP/Hello.cpp
@@ -1,6 +1,11 @@
+
+//C언어 -> C++언어로의 전환
+
 #include <iostream>
 
 
+//1. cout, cin
+//2. 변수 선언 위치 free
 int main()
 {
 	char a[6] = "Hello";
@@ -14,7 +19,7 @@ int main()
 
 
 
-//함수 오버로딩 : 함수명이 같고 파라미터가 다른 것 - 리턴타입은 신경x
+//3. 함수 오버로딩 : 함수명이 같고 파라미터가 다른 것 - 리턴타입은 신경x
 #include <iostream>
 
 void aaa()
@@ -36,7 +41,7 @@ void main()
 
 
 
-//함수 파라미터의 디폴트 값
+//4. 함수 파라미터의 디폴트 값
 
 #include <iostream>
 
@@ -57,7 +62,7 @@ void main()
 
 
 
-//inline함수 : 메모리의 상황에 따라 될 수도 안 될 수도 ..
+//5. inline함수 : 메모리의 상황에 따라 될 수도 안 될 수도 ..
 
 #include <iostream>
 
@@ -83,10 +88,156 @@ void main()
 
 
 
-//namespace
+
+
+
+//6. namespace[이름공간] '::범위지정연산자'
 
 #include <iostream>
 
-int a;
+namespace chanwoo //찬우라는 이름을 나타내는 공간 : 영역구분
+{
+	int hp = 50;
+	void attack()
+	{
+		std::cout << "침뱉어!" << std::endl;
+	}
+}
+
+namespace hyeongwoo //형우라는 이름을 나타내는 공간:
+{
+	int hp = 100;
+	void attack()
+	{
+		std::cout << "총 쏴!" << std::endl;
+	}
+}
 
 
+using chanwoo::hp; //찬우의 hp가 main hp가 되는것
+using chanwoo::attack; //찬우의 attack이 main attack이 되는 것
+						//함수라고 ()쓰지 말고 함수명만 쓰기
+
+
+using namespace chanwoo; //chanwoo로 지정된 namespace를 통째로 모두 풀어주는 방법
+
+
+void main()
+{
+	hp=50;
+	hyeongwoo::hp = 100;
+
+	attack();
+	hyeongwoo::attack();
+
+}
+
+
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin; //매번 3줄 쓰기 귀찮...
+
+using namespace std; //std의 모든 것을 풀어주는 것 - 개발자들이 선호
+
+void main()
+{
+	cout << "aaa" << endl;
+	cout << 10 << endl;
+
+	int a;
+	cin >> a;
+}
+
+
+
+
+//::의 사용 : 전역변수를 지역 내에 쓰는 방법
+#include <iostream>
+using namespace std;
+
+int a = 10; //전역변수
+
+void main()
+{
+	int a = 20; //지역변수
+
+	cout << ::a + a << endl; //전역변수 a를 지칭하는 방법 10+20=30
+}
+
+
+
+
+
+//7. 새로운 data type 논리값 : bool -> boolean
+#include <iostream>
+using namespace std;
+
+int a = 10;
+
+void main()
+{
+	bool a = 5 > 3; //true;
+	bool b = false;
+
+	if (a)
+	{
+		cout << "aaa";
+	}
+}
+
+
+
+
+
+
+void main()
+{
+	int a = 10;
+	int* p = &a;
+
+	cout << *p << endl;
+}
+
+
+//8. 참조자(Reference) : 별명 부여하기
+
+void main()
+{
+	int a = 10;
+	int& k=a; //참조자를 만드는 문법 - k는 a의 별명이다
+			//참조값이 넘어갔다.
+
+	int& s = a;
+	int& b = s;//별명에 별명을 붙이는 것이 가능
+
+	cout << k << endl;
+
+	k++; 
+
+	cout << a << endl;
+}
+
+
+//9. 동적 메모리 할당 (사용자 영역 정의)
+
+//생성: new
+//소멸: delete
+
+void main()
+{
+	int* p=new int; //원하는 자료형을 쓰는 것 - malloc은 byte수만 써서 자료형 저장X
+	//힙메모리에는 이름을 줄 수 없다.
+
+	*p = 10;
+
+	cout << *p << endl;
+	
+	delete p; //소멸
+}
+
+
+
+//10. string.h 대신 cstring의 사용
--- Sokoban/Sokoban/Sokoban.c
+++ Sokoban/Sokoban/Sokoban.c
@@ -1,4 +1,3 @@
-/*
 #include <stdio.h>
 #include <conio.h>
 #include <windows.h>
@@ -14,183 +13,14 @@
 #define DOT 2
 #define GOAL 3
 #define BRICK 4
+#define FENCE 5
 #define	ARROW 6
 
 
 
 void drawMap();
-int checkGameOver();
+int goalcheck();
 void keyPro();
-
-
-
-
-
-char map[11][20] = {
-	{5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5},
-	{5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5},
-	{5,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,5},
-	{5,0,0,0,0,1,2,2,2,2,1,0,0,0,0,0,0,0,0,5},
-	{5,0,0,0,0,1,4,2,2,4,1,1,1,1,0,0,0,0,0,5},
-	{5,0,1,1,1,1,2,2,2,2,6,2,2,1,1,1,1,1,0,5},
-	{5,0,1,2,2,4,2,2,4,2,2,2,2,2,2,3,3,1,0,5},
-	{5,0,1,2,2,2,1,2,2,1,1,2,2,2,2,3,3,1,0,5},
-	{5,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,5},
-	{5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5},
-	{5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5},
-
-};
-
-//현재 화살표의 위치좌표(배열의 index번호)
-int posY = 5;
-int posX = 10; 
-
-void main()
-{
-	while (1)
-	{
-		//화면 지우기
-		system("cls");
-		
-		//화면에 맵 그리기
-		drawMap();
-
-		//화면에 1이 없다면 종료
-		if (checkGameOver() ) //0이 아니면 참이기 때문에 이렇게 써도 된다
-		{
-			printf("\n\nStage Clear! \n\n");
-			break; //while문이 끝남
-		}
-
-		//방향키 입력받아서 map의 좌표 변경
-		keyPro();
-
-		//
-	}
-
-
-	
-}
-
-void drawMap()
-{
-	for (int y = 0; y < 11; y++)
-	{
-		for (int x = 0; x < 20; x++)
-		{
-			switch (map[y][x]) //전역변수로 처리했기 때문에 인식
-			{
-			case 0:
-				printf("  ");
-				break;
-			case 1:
-				printf("▦");
-				break;
-			case 2:
-				printf("ㆍ");
-				break;
-			case 3:
-				printf("ⓖ");
-				break;
-			case 4:
-				printf("■");
-				break;
-			case 5:
-				printf("≡");
-				break;
-			case 6:
-				printf("▶");
-				break;
-			}
-		}
-		printf("\n");
-	}
-}
-
-//게임종료를 검사하는 기능 함수
-int checkGameOver()
-{
-	int y, x;
-	for (int y = 0; y < 11; y++)
-	{
-		for (int x = 0; x < 20; x++)
-		{
-			if (map[y][x] == 1) return 0;
-		}
-
-	}
-	return 1;
-}
-
-
-void keyPro()
-	{
-		int key;
-		key = _getch();//방향키 입력을 받을 수 있는 함수
-		if (key == 224) key = _getch();
-
-
-		switch (key)
-		{
-		case UP:
-			//위칸이 비어있지 않은가?
-			if (map[posY - 1][posX] != DOT) return; //더 보기가 좋게 			
-			map[posY][posX] = DOT;
-			posY--;						//리턴을 만나는 순간 함수 종결
-			map[posY][posX] = ARROW;
-
-		case DOWN:
-			//아래칸이 비어있지 않은가?
-			if (map[posY + 1][posX] != DOT) return;   //매크로상수로 처리하면 더 보기 쉽게			
-			map[posY][posX] = DOT;
-			posY++;
-			map[posY][posX] = ARROW;
-			break;
-
-		case LEFT:
-			//왼쪽칸이 비어있지 않은가?
-			if (map[posY][posX - 1] != DOT) return;
-			map[posY][posX] = DOT;
-			posX--;
-			map[posY][posX] = ARROW;
-			break;
-
-		case RIGHT:
-			//오른쪽칸이 비어있지 않은가?
-			if (map[posY][posX + 1] != DOT) return;
-			map[posY][posX] = DOT;
-			posX++;
-			map[posY][posX] = ARROW;
-			break;
-
-		}
-}*/
-
-#include <stdio.h>
-#include <conio.h>
-#include <windows.h>
-
-
-#define UP 72
-#define DOWN 80
-#define LEFT 75
-#define RIGHT 77
-
-#define EMPTY 0
-#define WALL 1
-#define DOT 2
-#define GOAL 3
-#define BRICK 4
-#define	ARROW 6
-
-
-
-void drawMap();
-int checkGameOver();
-void keyPro();
-
-
-
 
 
 char map[11][20] = {
@@ -222,19 +52,18 @@ void main()
 		//화면에 맵 그리기
 		drawMap();
 
-		//화면에 1이 없다면 종료
-		if (checkGameOver()) //0이 아니면 참이기 때문에 이렇게 써도 된다
+		//방향키 입력받아서 map의 좌표 변경
+		keyPro();
+
+		//goal이 brick으로 채워지면 종료
+		if (goalcheck())
 		{
 			printf("\n\nStage Clear! \n\n");
 			break; //while문이 끝남
 		}
 
-		//방향키 입력받아서 map의 좌표 변경
-		keyPro();
 
-		//
 	}
-
 
 
 }
@@ -274,20 +103,6 @@ void drawMap()
 	}
 }
 
-//게임종료를 검사하는 기능 함수
-int checkGameOver()
-{
-	int y, x;
-	for (int y = 0; y < 11; y++)
-	{
-		for (int x = 0; x < 20; x++)
-		{
-			if (map[y][x] == GOAL) return 0;
-		}
-
-	}
-	return 1;
-}
 
 
 void keyPro()
@@ -300,88 +115,82 @@ void keyPro()
 	switch (key)
 	{
 	case UP:
-		if (map[posY - 1][posX] == WALL) return;
 
-		if ((map[posY - 1][posX] == BRICK) && (map[posY - 2][posX] == WALL)) return;
-
-		if (map[posY - 1][posX] == DOT)
+		if (map[posY - 1][posX] != WALL)
 		{
+			if (map[posY - 1][posX] == BRICK)
+			{
+				if (map[posY - 2][posX] == WALL || (map[posY - 2][posX] == BRICK)) return;
+
+				map[posY - 2][posX] = BRICK;
+			}
 			map[posY][posX] = DOT;
 			posY--;
 			map[posY][posX] = ARROW;
-		}
-		if ((map[posY - 1][posX] == BRICK) && (map[posY - 2][posX] == DOT))
-		{
-			map[posY][posX] = DOT;
-			posY--;
-			map[posY][posX] = ARROW;
-			map[posY-1][posX] = BRICK;
-		}
-		return;
+		}break;
 
 	case DOWN:
-		//아래칸이 비어있지 않은가?
-		if (map[posY + 1][posX] == WALL) return;
 
-		if ((map[posY + 1][posX] == BRICK) && (map[posY + 2][posX] == WALL)) return;
-
-		if (map[posY + 1][posX] == DOT)
+		if (map[posY + 1][posX] != WALL)
 		{
+			if (map[posY + 1][posX] == BRICK)
+			{
+				if (map[posY + 2][posX] == WALL || (map[posY - 2][posX] == BRICK)) return;
+
+				map[posY + 2][posX] = BRICK;
+			}
 			map[posY][posX] = DOT;
 			posY++;
 			map[posY][posX] = ARROW;
-		}
-		if ((map[posY + 1][posX] == BRICK) && (map[posY + 2][posX] == DOT))
-		{
-			map[posY][posX] = DOT;
-			posY++;
-			map[posY][posX] = ARROW;
-			map[posY + 1][posX] = BRICK;
-		}
-		return;
+		}break;
 
 	case LEFT:
-		//왼쪽칸이 비어있지 않은가?
-		if (map[posY][posX - 1] == WALL) return;
 
-		if ((map[posY][posX - 1] == BRICK) && (map[posY][posX - 2] == WALL)) return;
-
-		if (map[posY][posX - 1] == DOT)
+		if (map[posY][posX - 1] != WALL)
 		{
+			if (map[posY][posX - 1] == BRICK)
+			{
+				if (map[posY][posX - 2] == WALL || (map[posY][posX - 2] == BRICK)) return;
+
+				map[posY][posX - 2] = BRICK;
+			}
 			map[posY][posX] = DOT;
 			posX--;
 			map[posY][posX] = ARROW;
-		}
-		if ((map[posY][posX - 1] == BRICK) && (map[posY][posX - 2] == DOT))
-		{
-			map[posY][posX] = DOT;
-			posX--;
-			map[posY][posX] = ARROW;
-			map[posY][posX - 1] = BRICK;
-		}return;
-
-
-
+		}break;
 
 	case RIGHT:
-		//오른쪽칸이 비어있지 않은가?
-		if (map[posY][posX + 1] == WALL) return;
-
-		if ((map[posY][posX + 1] == BRICK) && (map[posY][posX + 2] == WALL)) return;
-
-		if (map[posY][posX + 1] == DOT)
+		if (map[posY][posX + 1] != WALL)
 		{
+			if (map[posY][posX + 1] == BRICK)
+			{
+				if (map[posY][posX + 2] == WALL || (map[posY][posX + 2] == BRICK)) return;
+
+				map[posY][posX + 2] = BRICK;
+			}
 			map[posY][posX] = DOT;
 			posX++;
 			map[posY][posX] = ARROW;
-		}
-		if ((map[posY][posX + 1] == BRICK) && (map[posY][posX + 2] == DOT || map[posY][posX + 2] ==GOAL))
-		{
-			map[posY][posX] = DOT;
-			posX++;
-			map[posY][posX] = ARROW;
-			map[posY][posX + 1] = BRICK;
-		}return;
+		}break;
 	}
 }
-//dot이 아닐때 
+
+
+
+int goalcheck()
+{
+	if (map[6][15] == !BRICK && map[6][15] != ARROW) map[6][15] = GOAL;
+	if (map[6][16] == !BRICK && map[6][16] != ARROW) map[6][16] = GOAL;
+	if (map[7][15] == !BRICK && map[7][15] != ARROW) map[7][15] = GOAL;
+	if (map[7][16] == !BRICK && map[7][15] != ARROW) map[7][16] = GOAL;
+
+
+	if (map[6][15] == BRICK && map[6][16] == BRICK && map[7][15] == BRICK && map[7][16] == BRICK)
+	{
+
+		system("cls");
+		drawMap();
+		return 1;
+	}
+	return 0;
+}
