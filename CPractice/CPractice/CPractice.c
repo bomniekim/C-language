67b45716bcff486b2c4b3eca365ad65a2cf8dbd5
BominKim <bomnie.kim@gmail.com>
2019-07-30 16:45:58 +0900

    C

---
+++ CPractice/CPractice/CPractice.c
@@ -0,0 +1,45 @@
+﻿
+#include <stdio.h>
+
+void main()
+
+{
+
+	int cnt = 0, x = 0, y = 0, n = 1, s = 1;
+
+
+
+	while (1)
+
+	{
+
+		n % 2 ? y++ : x++;
+
+
+
+		if (n >= 10) break;
+
+
+
+		for (int i = 0; i < n; i++)
+
+		{
+
+			x += s;
+
+			y -= s;
+
+		}
+
+		s = -s;
+
+		n++;
+
+	}
+
+	printf("%d, %d, %d, %d \n", x, y, s, n);
+
+}﻿
+
+
+
---
+++ Project2/Project2/c.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int arr1[] = { 1,2,3,4,5,6,7,8 };
+	int arr2[] = { 8,7,6,5,4,3,2,1 };
+
+	int args[8];
+
+	scanf("%d %d %d %d %d %d %d %d", &args);
+
+
+
+
+
+}
